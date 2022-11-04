dfcae6ce9d939e41b70c34a4ec4fb274011d7225
Abdirizak <abdirizaksigat99@gmail.com>
2022-11-04 15:49:57 +0300

    style: updated images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import "./footer.css"
-import Logo from "../assets/Zuri-logo.png";
-import I4G from "../assets/I4G.png";
-import Slack from "../assets/slack.png";
-import Github from "../assets/github.png";
-
 
 export default function Footer(){
   return(
   <div className="footer">
      <div className="footer-top">  
-       <a href="#" id="github" ><img src={Slack}/></a>
-       <a href="https://github.com/Abdirizaks12" id="github" ><img src={Github}/></a>
+       <a href="#" id="github" ><img src="../slack.png"/></a>
+       <a href="https://github.com/Abdirizaks12" id="github" ><img src="../github.png"/></a>
      
      </div>
 
      <div className="footer-bottom">
-         <img src={Logo}/>
+         <img src="../Zuri-logo.png"/>
          <p>HNG Internship 9 Frontend Task</p>
-         <img src={I4G}/>
+         <img src= "../I4G.png"/>
      </div>
   </div>)
 }
